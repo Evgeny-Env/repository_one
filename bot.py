99b3431389dad7e1563f273142250c240b22ffa4
Evgeny-Env <69090128+Evgeny-Env@users.noreply.github.com>
2020-09-09 05:21:04 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+
+from telebot import types
+
+bot = telebot.TeleBot(token = '1355474133:AAFBY-tC9aRRLPQftVh49IDPyVG7t91JmkA')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=-1)
